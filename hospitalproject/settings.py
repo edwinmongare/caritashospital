8f33d3fc31d60c3b7b7fa4ee4d8938c05a8ad076
edwinmongare <edwinmongare15@gmail.com>
2019-10-16 02:17:12 +0300

    settings update

--- hospitalproject/settings.py
+++ hospitalproject/settings.py
@@ -130,7 +130,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 
