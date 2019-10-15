f52ad390d9b821fabd5ec18635f5ba4321820bd6
edwinmongare <edwinmongare15@gmail.com>
2019-10-16 01:58:24 +0300

    settings update

--- hospitalproject/settings.py
+++ hospitalproject/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 
