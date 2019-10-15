3f2d87d0dadac388e1e69d09ff8e6eb3c4c806fa
edwinmongare <edwinmongare15@gmail.com>
2019-10-16 02:25:18 +0300

    settings update

--- hospitalproject/settings.py
+++ hospitalproject/settings.py
@@ -121,17 +121,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
 
-# django_heroku.settings(locals())
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+django_heroku.settings(locals())
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
-
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
